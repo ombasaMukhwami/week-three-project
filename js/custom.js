d6419ebc76cf7a7d1d752191d57e12a8a7bf2711
omukhwami <omukhwami@safaricom.co.ke>
2022-11-14 19:51:55 +0300

    design on it way

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,28 @@
 $(document).ready(function(){
-    $("#btnTest").click(function(){
-        alert('Testing 123');
-      });
+    
+    $("#name").keyup(function() {
+        var name = $("#name").val();
+        if (name === '') {
+            $("#name").css("border", "2px solid red");
+        }
+    });
 });
 
+
+function errorMessage(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    //add error message inside small
+    small.innerText = message;
+
+    //add error class
+    formControl.className = 'form-control error';
+}
+
+function successMessage(input) {
+    const formControl = input.parentElement;
+
+    //add error class
+    formControl.className = 'form-control success';
+}
